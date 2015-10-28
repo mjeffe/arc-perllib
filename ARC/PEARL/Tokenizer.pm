e662e6c6122b009f84687b72e7618a37fc956203
mjeffe <mjeffe@6c1b6914-55f0-4e17-93f1-3bc28c16f5fc>
2015-10-28 14:24:10 +0000

    Committing quite a few changes. There is still a lot of commented out code that will need to be cleaned up, and detokenizing still does not work. This is more of a checkpoint. 1) renamed rmc tokens to aric tokens 2) modified tokenizing:    - xo token has version and map_id header    - aric tokens have field name code but no map_id    - do portion is merged into xo token so output is a single token 3) moved set_rmc_options() code into init_tokenizer() 4) modified init_tokenizer() to document and set defaults for all possible    options, and merge input options with defaults 5) moved set default of rmc_max_char_map_length into init_tokenizer() 6) made map_id a global, randomly selected at init time for the entire run 7) made rmc_map a global

--- ARC/PEARL/Tokenizer.pm
+++ ARC/PEARL/Tokenizer.pm
@@ -14,7 +14,7 @@ require Exporter;
 our @ISA = qw(Exporter);
 # export functions
 our @EXPORT = qw(init_tokenizer tokenize_strings detokenize_strings);
-our @EXPORT_OK = qw(tok_encrypt_rmc tok_decrypt_rmc tok_encrypt_xo tok_decrypt_xo);
+our @EXPORT_OK = qw(tok_encrypt_rmc tok_decrypt_rmc tok_encrypt_xo tok_decrypt_xo tok_encrypt_do tok_decrypt_do);
 
 use strict;
 use warnings;
@@ -22,6 +22,7 @@ use Crypt::CBC;
 #use Digest;
 #use Digest::MD5 qw(md5_hex);
 #use Digest::SHA qw(sha512_hex);
+use List::Util 'shuffle';
 use Data::Dumper;
 use ARC::Common;
 use ARC::Common qw($E);
@@ -29,14 +30,15 @@ use ARC::Common qw($E);
 # prototypes
 sub add_noise($);
 sub build_rmc_map();
-sub set_rmc_options();
-sub tok_encrypt_rmc($$);
+sub tok_encrypt_rmc($$$);
 sub tok_decrypt_rmc($);
 sub tok_encrypt_xo($);
 sub tok_decrypt_xo($);
+sub tok_encrypt_do($);
+sub tok_decrypt_do($);
 # exportable
 sub init_tokenizer(%);
-sub tokenize_strings($);
+sub tokenize_strings($$);
 sub detokenize_strings($);
 # obsolete
 sub random_str($@);
@@ -44,85 +46,182 @@ sub generate_map_table($$);
 
 # globals
 my %opts = ();
+my %rmc_map = ();
+my %rmc_rmap = ();
+my $map_id = '';
 
 
 
 
 # ---------------------------------------------------------------------------
+# Initialize the tokenizer
 # ---------------------------------------------------------------------------
 sub init_tokenizer(%) {
    my ($href) = @_;
-   %opts = %$href;
-   #print "TOK INIT:\n" . Dumper(\%opts) . "\n";
+   #%opts = %$href;
+   #$opts{'xo-do-delimiter'} = 'h';
 
-   set_rmc_options();
+   # document the options we expect and set defaults
+   my %defaults = (
+      'delimiter'       => '|', 
+      'output-aric'     => 0,
+      'seperate-do'     => 0,
+      'xo-do-delimiter' => 'h',
+      'keys' => {
+         'ptk_version'              => undef,
+         'aric_version'             => undef,
+         'aric_token_delimiter'     => '|',
+         'aric_header_delimiter'    => ':',
+         'rmc_salt'                 => undef,
+         'rmc_input_domain'         => undef,
+         'rmc_max_char_map_length'  => 4,
+         'xo_cipher_alog'           => 'Rijndael',
+         'xo_cipher_key'            => undef,
+         'xo_cipher_salt'           => undef,
+         'do_cipher_alog'           => 'Rijndael',
+         'do_cipher_key'            => undef,
+         'iv'                       => undef,
+      },
+   );
+
+   # override defaults with input parms
+   %opts = (%defaults, %$href);
+   %{$opts{keys}} = (%{$defaults{keys}}, %{$href->{keys}});
+   #print "ARC::Tokenizer init opts:\n" . Dumper(\%opts) . "\n";
+
+   # check for required options
+   die("$E: aric_version is not defined in the key file!\n")
+      unless( defined($opts{keys}{aric_version}) );
+   die("$E: xo_cipher_key is not defined in the key file!\n")
+      unless( defined($opts{keys}{xo_cipher_key}) );
+   die("$E: xo_cipher_salt is not defined in the key file!\n")
+      unless( defined($opts{keys}{xo_cipher_salt}) );
+
+   # generate a 16 byte Initialization Vector from "salt"
+   #$opts{'keys'}{'iv'} = substr(Digest::MD5::md5_hex($opts{keys}{xo_cipher_salt}), 0, 16);
+
+   # each version of rmc has different options. Set those here
+   if ( $opts{keys}{aric_version} == 3 ) {
+      die("$E: rmc_salt is not defined in the key file!\n") if ( ! defined($opts{keys}{rmc_salt}) );
+      $opts{keys}{aric_header_delimiter} = ':';
+      $opts{keys}{aric_token_delimiter} = '|';
+   }
+   else {
+      die("$E: unknown aric_version\n");
+   }
+
+   # build the in-memory rmc maps
    build_rmc_map();
+
+   # pick a random map key to use for this instantiation
+   # This could also be done in tokenize_strings() for greater randomness
+   my @keys = keys %rmc_map;
+   $map_id = $keys[int(rand(scalar @keys))];
 }
 
 
 # ---------------------------------------------------------------------------
-# tokenzie every string in the array ref
+# tokenzie every string in the array
 #
-# OJO: For efficiency we may want to pick a random map_id once in init and use
-# the same key for the entire invokation? Would not be as random, but...
+#  strs_ref       : array ref of strings
+#  flds_ref       : array ref of field names
+#
+# OJO: we could generate map_id here for greater randomness
 # ---------------------------------------------------------------------------
-sub tokenize_strings($) {
-   my ($arrayref) = @_;
-
-   # pick a random map key to use for all strings in this batch
-   my @keys = keys $opts{rmc_map};
-   my $map_id = $keys[int(rand(scalar @keys))];
+sub tokenize_strings($$) {
+   my ($strs_ref, $flds_ref) = @_;
 
    # generate aric tokens (currently using RMC)
-   my @arr = map { tok_encrypt_rmc($_, $map_id); } @$arrayref;
+   #my @arr = map { tok_encrypt_rmc($_, $map_id); } @$strs_ref;
+   my @arr = ();
+   for (my $i = 0; $i < scalar @$strs_ref; $i++ ) {
+      push(@arr, tok_encrypt_rmc($strs_ref->[$i], $map_id, substr($flds_ref->[$i],0,2)));
+   }
    if ( $opts{'output-aric'} ) { return @arr; }
 
    # concatenate all aric tokens
-   my $aric = join($opts{keys}{rmc_token_delimiter}, @arr);
+   my $aric = join($opts{keys}{aric_token_delimiter}, @arr);
 
    # aric will now put on the XO armor
-   my @xo = map { tok_encrypt_xo($_); } ($aric);
+   #
+   # token header format = VVS*:
+   # where:
+   #   VV   = arci version number in hex
+   #   S*   = variable length string, map table key (substr of DO password hash)
+   # 
+   # NOTE: what follows aric version in the header, should be version dependent. For
+   #   now this is unimplemented.
+   #
+   my $xo = sprintf("%02X", $opts{keys}{aric_version}) . $map_id . $opts{keys}{aric_header_delimiter} 
+         . tok_encrypt_xo($aric);
 
-   return @xo;
+   # generate the DO token
+   if ( $opts{'seperate-do'} ) {
+      return ($xo, tok_encrypt_do(join($opts{delimiter}, $strs_ref)));
+   }
+
+   return ($xo . $opts{'xo-do-delimiter'} . tok_encrypt_do(join($opts{delimiter}, $strs_ref)));
 }
 
 # ---------------------------------------------------------------------------
 # detokenzie every string in the array ref
+#
+# See tokenize_strings() for token header notes
 # ---------------------------------------------------------------------------
 sub detokenize_strings($) {
-   my ($arrayref) = @_;
+   #my ($arrayref) = @_;
+   my ($token) = @_;
+
+   # pull header from token and parse out it's two values
+   my ($hdr, $tok) = split(quotemeta($opts{keys}{aric_header_delimiter}), $token);
+   my $map_version = hex(substr($hdr,0,2));  # first two bytes, in hex
+   my $map_id      = substr($hdr,2); # map key
+
+   if ( $map_version != $opts{keys}{aric_version} ) {
+      die("$E: Unable to decrypt token: aric_version missmatch.\n"
+         ."    key file aric_version = " . $opts{keys}{aric_version} 
+         . ", token aric_version = $map_version\n");
+   }
+
+   # seperate xo and do tokens
+   my ($xo, $do) = split($opts{'xo-do-delimiter'}, $tok);
 
    # aric will now take off the XO armor
-   my @arr = map { tok_decrypt_xo($_); } @$arrayref;
+   #my @arr = map { tok_decrypt_xo($_); } @$arrayref;
+   my $aric = tok_decrypt_xo($xo);
 
    # parse out the aric tokens
-   my @aric = split(quotemeta($opts{keys}{rmc_token_delimiter}), $arr[0]);
+   my @aric = split(quotemeta($opts{keys}{aric_token_delimiter}), $aric);
+   return @aric;
 
    # decrypt aric tokens back to plaintext
-   my @plaintext = map { tok_decrypt_rmc($_); } @aric;
+   #my @plaintext = map { tok_decrypt_rmc($_); } @aric;
 
-   return @plaintext;
+   #return @plaintext;
 }
 
 
 # ---------------------------------------------------------------------------
-# create RMC token (Random Map Cipher)
-#
-# For each token
+# create RMC (Random Map Cipher) token
 # ---------------------------------------------------------------------------
-sub tok_encrypt_rmc($$) {
-   my ($plaintext, $map_id) = @_;
+sub tok_encrypt_rmc($$$) {
+   my ($plaintext, $map_id, $fieldname) = @_;
 
-   # token header format = VVS*:
+   # $map_id can also be generated here rather than init() for more randomness
+
+   # token header format = FF:
    # where:
-   #   VV   = map table version number in hex
-   #   S*   = variable length string, map table key (substr of DO password hash)
-   my $token = sprintf("%02X", $opts{keys}{rmc_version}) . $map_id . $opts{keys}{rmc_delimiter};
+   #   FF   = field name code
+   my $token = $fieldname . $opts{keys}{aric_header_delimiter};
+   # NOTE: output-aric is for debugging, so the map_id is included
+   if ( $opts{'output-aric'} ) {
+      $token = $fieldname . $map_id . $opts{keys}{aric_header_delimiter};
+   }
 
    # character by character, replace original with mapped value from map_id row
    foreach my $c ( split('', uc($plaintext)) ) {
       # TODO: Need to print the record this is from to log file and warn that character $c was dropped
-      $token .= $opts{rmc_map}{$map_id}{$c} || '';  # drop character if not found in map
+      $token .= $rmc_map{$map_id}{$c} || '';  # drop character if not found in map
    }
 
    return $token;
@@ -138,14 +237,14 @@ sub tok_decrypt_rmc($) {
    my ($token) = @_;
 
    # pull header from token and parse out it's two values
-   my ($hdr, $tok) = split(quotemeta($opts{keys}{rmc_delimiter}), $token);
+   my ($hdr, $tok) = split(quotemeta($opts{keys}{aric_header_delimiter}), $token);
    my $map_version = hex(substr($hdr,0,2));  # first two bytes, in hex
    my $map_id      = substr($hdr,2); # map key
 
-   if ( $map_version != $opts{keys}{rmc_version} ) {
-      die("$E: Unable to decrypt token: rmc_version missmatch.\n"
-         ."    key file rmc_version = " . $opts{keys}{rmc_version} 
-         . ", token rmc_version = $map_version\n");
+   if ( $map_version != $opts{keys}{aric_version} ) {
+      die("$E: Unable to decrypt token: aric_version missmatch.\n"
+         ."    key file aric_version = " . $opts{keys}{aric_version} 
+         . ", token aric_version = $map_version\n");
    }
 
    # length of rmc map id is the length strings it maps to or from
@@ -154,7 +253,7 @@ sub tok_decrypt_rmc($) {
    # chunk the string into equal parts of $len length
    my $plaintext;
    foreach my $chunk ( unpack("(a$len)*", $tok) ) {
-      $plaintext .= $opts{rmc_rmap}{$map_id}{"$chunk"}; 
+      $plaintext .= $rmc_rmap{$map_id}{"$chunk"}; 
    }
 
    return $plaintext;
@@ -201,25 +300,44 @@ sub tok_decrypt_xo($) {
    return $cipher->decrypt_hex($ciphertext);
 }
 
-
 # ---------------------------------------------------------------------------
-# each version of rmc has different options. Set those here
-# 
-# MUST run AFTER parse_key_file().
+# create DO (Data Owner) token 
 # ---------------------------------------------------------------------------
-sub set_rmc_options() {
-   # we MUST run AFTER parse_key_file().
-   die("$E: rmc_version is not defined in the key file!\n")
-      unless( exists($opts{keys}{rmc_version}) );
+sub tok_encrypt_do($) {
+   my ($plaintext) = @_;
 
-   if ( $opts{keys}{rmc_version} == 3 ) {
-      $opts{keys}{rmc_delimiter} = ':';
-      $opts{keys}{rmc_token_delimiter} = '|';
-   }
-   else {
-      die("$E: unknown rmc_version\n");
-   }
+   my $cipher = Crypt::CBC->new(
+         -key           => $opts{keys}{do_cipher_key},
+         -literal_key   => 0,  # treat -key as a passphrase, not the literal encryption key
+         -cipher        => $opts{keys}{do_cipher_algo},
+         #-salt          => $cipher_salt,
+         #-iv            => $opts{keys}{cipher_salt},
+         #-iv            => $opts{keys}{iv},
+         #-header        => 'none',
+   );
+
+   return $cipher->encrypt_hex($plaintext);
 }
+
+
+# ---------------------------------------------------------------------------
+# decrypt DO (Data Owner) token 
+# ---------------------------------------------------------------------------
+sub tok_decrypt_do($) {
+   my ($ciphertext) = @_;
+
+   my $cipher = Crypt::CBC->new(
+         -key           => $opts{keys}{do_cipher_key},
+         -literal_key   => 0,  # treat -key as a passphrase, not the literal encryption key
+         -cipher        => $opts{keys}{do_cipher_algo},
+         #-salt          => $cipher_salt,
+         #-iv            => $opts{keys}{iv},
+         #-header        => 'none',
+   );
+
+   return $cipher->decrypt_hex($ciphertext);
+}
+
 
 # ---------------------------------------------------------------------------
 # Add more bytes to the noise map
@@ -231,15 +349,19 @@ sub add_noise($) {
 
 # ---------------------------------------------------------------------------
 # Build the in-memory random map dictionary
+#
+# Currently we generate one map for each $len. We could however, generate
+# multiple maps for each $len, which would give us much greater "randomness".
 # ---------------------------------------------------------------------------
 sub build_rmc_map() {
    dbg(2, "Generating rmc maps\n");
 
-   # default char_map max length
-   my $max_strlen = 4;
-   if ( defined($opts{keys}{rmc_max_char_map_length}) ) {
-      $max_strlen = int($opts{keys}{rmc_max_char_map_length});
-   }
+   ## default char_map max length
+   #my $max_strlen = 4;
+   #if ( defined($opts{keys}{rmc_max_char_map_length}) ) {
+   #   $max_strlen = int($opts{keys}{rmc_max_char_map_length});
+   #}
+   my $max_strlen = int($opts{keys}{rmc_max_char_map_length});
 
    # default to all 7-bit ASCII printable characters 
    # see http://perldoc.perl.org/perlrecharclass.html#POSIX-Character-Classes
@@ -272,8 +394,6 @@ sub build_rmc_map() {
    #     map string (n bytes long) => input domain character   # for reverse maps
    #  }
    $opts{keys}{do_pw_hash} = Digest::MD5::md5_hex($opts{keys}{do_cipher_key});
-   my %rmc_map = ();
-   my %rmc_rmap = ();
    my $i = 0;
    foreach my $len ($min_strlen .. $max_strlen) {
       dbg(3, "building rmc map for len $len\n");
@@ -298,8 +418,8 @@ sub build_rmc_map() {
    }
    #print "MAP: " . Dumper(\%rmc_map);
    #print "RMAP: " . Dumper(\%rmc_rmap);
-   $opts{rmc_map} = \%rmc_map;
-   $opts{rmc_rmap} = \%rmc_rmap;
+   #$opts{rmc_map} = \%rmc_map;
+   #$opts{rmc_rmap} = \%rmc_rmap;
 }
 
 # ---------------------------------------------------------------------------
